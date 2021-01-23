4d5fa067be8788850955268403065591b3d85979
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-01-23 22:57:33 +0530

    practic

--- practic/src/customStore.js
+++ practic/src/customStore.js
@@ -1,13 +1,27 @@
-function createStore() {
+import reducer from './reducer';
+
+function createStore(reducer) {
   let state;
+  let listeners = [];
+
+  function subscribe(listeners) {
+    listeners.push(listeners);
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    for (let i = 0; i < listeners.length; i++) listeners[i]();
+  }
 
   function getState() {
     return state;
   }
 
   return {
+    subscribe,
+    dispatch,
     getState,
   };
 }
 
-export default customStore();
+export default customStore(reducer);
--- practic/src/index.js
+++ practic/src/index.js
@@ -1,1 +1,8 @@
 import store from './customStore';
+import * as actions from './actionTypes';
+
+store.subscribe(() => {
+  console.log('Store Changed! ');
+});
+
+store.dispatch(actions.BUG_ADDED('Bug 1'));
